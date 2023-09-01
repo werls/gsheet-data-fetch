940a567ee8526c26cbf2abcf32e0bbeaef3b9032
Werllen Castro <hi@werls.xyz>
2023-09-01 16:16:31 -0300

    update

--- get-data.js
+++ get-data.js
@@ -9,29 +9,29 @@ const fs = require('fs');
  * @returns {array} data
  */
 async function getData(filename = 'gs-data.json') {
-    console.log('Trying to access Google Sheet data...')
+    // console.log('Trying to access Google Sheet data...')
     try {
         await doc.useServiceAccountAuth({
             client_email: process.env.CLIENT_EMAIL,
             private_key: process.env.API_KEY.replace(/\\n/g, '\n'),
         });
         await doc.loadInfo();
-        console.log('Got access to Google Sheet data!')
+        // console.log('Got access to Google Sheet data!')
     } catch (error) {
-        console.log('ERROR: Could not access Google Sheet data!')
+        // console.log('ERROR: Could not access Google Sheet data!')
         console.log(error)
         return
     }
 
-    console.log('Getting the sheet...')
+    // console.log('Getting the sheet...')
     const sheet = doc.sheetsByIndex[0];
-    console.log('Got sheet!')
+    // console.log('Got sheet!')
     // const sheet = doc.sheetsByTitle['Sheet1'];
-    console.log('Getting sheet data...')
+    // console.log('Getting sheet data...')
     const rows = await sheet.getRows();
-    console.log('Got sheet data!')
+    // console.log('Got sheet data!')
 
-    console.log('Processing data...')
+    // console.log('Processing data...')
     const data = rows.map(row => {
         const obj = {};
         sheet.headerValues.forEach(header => {
@@ -40,12 +40,12 @@ async function getData(filename = 'gs-data.json') {
         });
         return obj;
     });
-    console.log('Data processed!')
+    // console.log('Data processed!')
 
     // console.log('Writing data to file...')
     // fs.writeFileSync(filename, JSON.stringify(data));
     // console.log('Data written to file!')
-    
+    console.log(JSON.stringify(data))
     return JSON.stringify(data)
 }
 
