2c21d78cb497fdd13150e3a3a02e510d9c76c95e
Werllen Castro <werllen@gmail.com>
2023-09-01 14:01:28 -0300

    Update get-data.js

--- get-data.js
+++ get-data.js
@@ -1,5 +1,44 @@
-const CLIENT_ID = process.env.CLIENT_ID;
-const API_KEY = process.env.API_KEY;
-const SHEET_ID = process.env.SHEET_ID;
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
 
-console.log(SHEET_ID)
+/**
+ * a function to get data from a Google Sheet
+ * @returns {array} data
+ */
+async function getData() {
+    try {
+        await doc.useServiceAccountAuth({
+            client_email: process.env.CLIENT_EMAIL,
+            private_key: process.env.API_KEY.replace(/\\n/g, '\n'),
+        });
+        await doc.loadInfo(); 
+        console.log('--- Google Sheet data accessed ---')
+    } catch (error) {
+        console.log('! Cant access Google Sheet data')
+        console.log(error)
+        return
+    }
+
+    console.log('--- Getting data from Google Sheet ---')
+    const sheet = doc.sheetsByIndex[0];
+    console.log('--- Got data from Google Sheet ---')
+    // const sheet = doc.sheetsByTitle['Sheet1'];
+    console.log('--- Getting sheet data ---')
+    const rows = await sheet.getRows();
+    console.log('--- Got sheet data ---')
+
+    console.log('--- Processing data ---')
+    const data = rows.map(row => {
+        const obj = {};
+        sheet.headerValues.forEach(header => {
+            const value = row[header];
+            obj[header] = value !== null ? value : NaN;
+        });
+        return obj;
+    });
+    console.log('--- Data processed ---')
+
+    return data
+}
+
+console.log(getData())
