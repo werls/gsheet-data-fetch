a40da3966eed11ace64a95acd26c7497acdd7d52
Werllen Castro <werllen@gmail.com>
2023-09-01 13:58:55 -0300

    Update get-data.js

--- get-data.js
+++ get-data.js
@@ -1,1 +1,5 @@
-console.log('getting google sheet data...')
+const CLIENT_ID = process.env.CLIENT_ID;
+const API_KEY = process.env.API_KEY;
+const SHEET_ID = process.env.SHEET_ID;
+
+console.log(SHEET_ID)
